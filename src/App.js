ba393594b9fcba34e20634c18bb53678e0fb9a2e
KaloyanDanev <kaloian4obg99@gmail.com>
2020-06-30 10:17:47 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import styles from './App.module.css';
+
+class App extends React.Component {
+
+
+
+    render() {
+        return (
+            <div className={styles.container}>
+                Home
+            </div>
+        );
+    }
+}
+
+export default App;
