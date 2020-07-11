1972f9e8cb35caaf851399916ffca677af499392
KaloyanDanev <kaloian4obg99@gmail.com>
2020-07-11 22:01:07 +0300

    Updates

---
+++ precache-manifest.94acc678b98008136e89b49a43c2e4c1.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe23b5b1c6ed73b0d0c36d4a4e33a7bd",
+    "url": "/KaloyansPizza/index.html"
+  },
+  {
+    "revision": "c7a82dc4957e31ad2224",
+    "url": "/KaloyansPizza/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "16859a4cb9d5b5eeb7ea",
+    "url": "/KaloyansPizza/static/css/main.f9866c78.chunk.css"
+  },
+  {
+    "revision": "c7a82dc4957e31ad2224",
+    "url": "/KaloyansPizza/static/js/2.fb246cf1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/KaloyansPizza/static/js/2.fb246cf1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "16859a4cb9d5b5eeb7ea",
+    "url": "/KaloyansPizza/static/js/main.75477bde.chunk.js"
+  },
+  {
+    "revision": "381315eaa2c6beeaddb7",
+    "url": "/KaloyansPizza/static/js/runtime-main.b85aec9f.js"
+  },
+  {
+    "revision": "c12870d5b5ffa2aa3091d622607043d1",
+    "url": "/KaloyansPizza/static/media/aquafina-water.c12870d5.jpg"
+  },
+  {
+    "revision": "6fa038376a5bf0f134a8eb46047e5254",
+    "url": "/KaloyansPizza/static/media/background.6fa03837.jpg"
+  },
+  {
+    "revision": "086c57bf2834d21afaaf497204850c37",
+    "url": "/KaloyansPizza/static/media/bacon.086c57bf.jpg"
+  },
+  {
+    "revision": "24d4f6910df9fb71fbfd1eb56737e268",
+    "url": "/KaloyansPizza/static/media/banana-peppers.24d4f691.jpg"
+  },
+  {
+    "revision": "05b20629064952890176ac7f26a0b0be",
+    "url": "/KaloyansPizza/static/media/beef.05b20629.jpg"
+  },
+  {
+    "revision": "beb8ebc48319f65f7e5e5783c481678e",
+    "url": "/KaloyansPizza/static/media/canadian-bacon.beb8ebc4.jpg"
+  },
+  {
+    "revision": "351f259125bfae195d1c4e3ed70ca445",
+    "url": "/KaloyansPizza/static/media/cheese.351f2591.jpg"
+  },
+  {
+    "revision": "9f028177765275a0923ede13ab935f87",
+    "url": "/KaloyansPizza/static/media/chicken.9f028177.jpg"
+  },
+  {
+    "revision": "8bbcf2b572b687aefbfa7aeb7a93ab1d",
+    "url": "/KaloyansPizza/static/media/chloe.8bbcf2b5.jpg"
+  },
+  {
+    "revision": "1836581604d3c6b36840f4d41fc7d498",
+    "url": "/KaloyansPizza/static/media/chocolate-chip-brownie.18365816.jpg"
+  },
+  {
+    "revision": "cd93e620015fd05db486138162c21134",
+    "url": "/KaloyansPizza/static/media/chocolate-chip-cookie.cd93e620.jpg"
+  },
+  {
+    "revision": "ecd7ca693d2cd32026a92b420c18df35",
+    "url": "/KaloyansPizza/static/media/christine.ecd7ca69.jpg"
+  },
+  {
+    "revision": "aaaa75f6976664c894bddc349f528c75",
+    "url": "/KaloyansPizza/static/media/cinn-pull-aparts.aaaa75f6.jpg"
+  },
+  {
+    "revision": "7f6f9b5ac60d631489436ceacdaa511f",
+    "url": "/KaloyansPizza/static/media/crush.7f6f9b5a.jpg"
+  },
+  {
+    "revision": "0c0d0c1aae0e0156a162d06cfcbe7e1f",
+    "url": "/KaloyansPizza/static/media/diet-pepsi.0c0d0c1a.jpg"
+  },
+  {
+    "revision": "2f8444d1bd8e6c441504be39752c89eb",
+    "url": "/KaloyansPizza/static/media/donut-holes.2f8444d1.jpg"
+  },
+  {
+    "revision": "7e68e65bde42697166021051cb7cc96e",
+    "url": "/KaloyansPizza/static/media/extra-cheese.7e68e65b.jpg"
+  },
+  {
+    "revision": "5fab311296f95e1e3de4530edc0d9105",
+    "url": "/KaloyansPizza/static/media/garden.5fab3112.jpg"
+  },
+  {
+    "revision": "0a17305e16d8b92c0a0744eaffd26f5c",
+    "url": "/KaloyansPizza/static/media/green-peppers.0a17305e.jpg"
+  },
+  {
+    "revision": "e4362a83d20f727455ce321ee18f27e8",
+    "url": "/KaloyansPizza/static/media/grilled-chicken.e4362a83.jpg"
+  },
+  {
+    "revision": "690c33ec23d9804616e8d7dd2188c2e8",
+    "url": "/KaloyansPizza/static/media/jalapeno-peppers.690c33ec.jpg"
+  },
+  {
+    "revision": "463e064bafe8c580f20af15ff2950fe1",
+    "url": "/KaloyansPizza/static/media/mtn-dew.463e064b.jpg"
+  },
+  {
+    "revision": "487cda7afa057004dac54d4563b66bab",
+    "url": "/KaloyansPizza/static/media/mug.487cda7a.jpg"
+  },
+  {
+    "revision": "770d39e24705a746ccc9c2b486d4dbbc",
+    "url": "/KaloyansPizza/static/media/mushrooms.770d39e2.jpg"
+  },
+  {
+    "revision": "d8eb1528ce1984ad6913c6720083113b",
+    "url": "/KaloyansPizza/static/media/olives.d8eb1528.jpg"
+  },
+  {
+    "revision": "5cfc12074475e43d71c20ebceb02329d",
+    "url": "/KaloyansPizza/static/media/onions.5cfc1207.jpg"
+  },
+  {
+    "revision": "2621654ccc6a3dd347f6375df177b635",
+    "url": "/KaloyansPizza/static/media/parmesan.2621654c.jpg"
+  },
+  {
+    "revision": "190e241189ef5e0af474956326403a3a",
+    "url": "/KaloyansPizza/static/media/pepperoni.190e2411.jpg"
+  },
+  {
+    "revision": "1dcbac62245736740c40eed2b45eefe4",
+    "url": "/KaloyansPizza/static/media/pepperoni.1dcbac62.jpg"
+  },
+  {
+    "revision": "252f78cdb2a7da087302c441200bb1b5",
+    "url": "/KaloyansPizza/static/media/pepsi.252f78cd.jpg"
+  },
+  {
+    "revision": "cfa027b3988687891f976361ef368df4",
+    "url": "/KaloyansPizza/static/media/pineapple.cfa027b3.jpg"
+  },
+  {
+    "revision": "cffafc435c4d289c0a36da835e7dffbe",
+    "url": "/KaloyansPizza/static/media/rikki.cffafc43.jpg"
+  },
+  {
+    "revision": "9b9626670c9ce74a36fac25a603fce6e",
+    "url": "/KaloyansPizza/static/media/salami.9b962667.jpg"
+  },
+  {
+    "revision": "7f40fdd0f6a771c938089f9f7550ef2b",
+    "url": "/KaloyansPizza/static/media/sausage.7f40fdd0.jpg"
+  },
+  {
+    "revision": "ae744363ea4394bf928294dc671d6377",
+    "url": "/KaloyansPizza/static/media/sausage.ae744363.jpg"
+  },
+  {
+    "revision": "6e6d9ec2dcdfbaaa12b8aa73e70e912a",
+    "url": "/KaloyansPizza/static/media/sierra.6e6d9ec2.jpg"
+  },
+  {
+    "revision": "a071b8fceaa3f8d77bc6290b7a8a4c97",
+    "url": "/KaloyansPizza/static/media/spicy-italian.a071b8fc.jpg"
+  },
+  {
+    "revision": "101a864c5848cc3cd9839827d448ade2",
+    "url": "/KaloyansPizza/static/media/three-cheese.101a864c.jpg"
+  },
+  {
+    "revision": "cef1232e0d66a32a50fed3a0342a459b",
+    "url": "/KaloyansPizza/static/media/tomatoes.cef1232e.jpg"
+  }
+]);
