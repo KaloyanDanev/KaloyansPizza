8e7fbad023d8007eb56ca9a041e4343b1013cf9a
KaloyanDanev <kaloian4obg99@gmail.com>
2020-07-11 22:48:05 +0300

    Link to menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from '../../images/brand/logo_transparent.png'
 import ShoppingCart from "../ShoppingCart/ShoppingCart";
+import { Link } from "react-router-dom";
 
 class Navbar extends React.Component {
     render() {
@@ -13,7 +14,7 @@ class Navbar extends React.Component {
                         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                             <div className="navbar-nav">
                                 <a href="/#how-it-works" className="nav-item nav-link font-weight-bold active" >HOW IT WORKS</a>
-                                <a href="/menu" className="nav-item nav-link font-weight-bold active">MENU</a>
+                                <Link to="menu" className="nav-item nav-link font-weight-bold active">MENU</Link>
                             </div>
                         </div>
                         <a href="/" className="btn btn-lg orderBtn" role="button">Start Order</a>
