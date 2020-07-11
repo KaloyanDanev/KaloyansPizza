d66671e5396534317992594deeba58dca7c8dac5
KaloyanDanev <kaloian4obg99@gmail.com>
2020-07-11 22:43:55 +0300

    without link to and gh- page

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
 import logo from '../../images/brand/logo_transparent.png'
 import ShoppingCart from "../ShoppingCart/ShoppingCart";
-import { Link } from "react-router-dom";
 
 class Navbar extends React.Component {
     render() {
         return (
                 <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-white" id="main-navbar">
                     <div className="container">
-                        <Link to="/" className="navbar-brand">
+                        <a href="/" className="navbar-brand">
                             <img src={logo} className="d-inline-block align-top" alt="logo"/>
-                        </Link>
+                        </a>
                         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                             <div className="navbar-nav">
                                 <a href="/#how-it-works" className="nav-item nav-link font-weight-bold active" >HOW IT WORKS</a>
-                                <Link to="/menu" className="nav-item nav-link font-weight-bold active">MENU</Link>
+                                <a href="/menu" className="nav-item nav-link font-weight-bold active">MENU</a>
                             </div>
                         </div>
-                        <Link to="/" className="btn btn-lg orderBtn" role="button">Start Order</Link>
+                        <a href="/" className="btn btn-lg orderBtn" role="button">Start Order</a>
                     <ShoppingCart/>
                     </div>
                 </nav>
