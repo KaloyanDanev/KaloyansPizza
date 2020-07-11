374180449eba64dc4ec6eb326ba9f688e1e127dd
KaloyanDanev <kaloian4obg99@gmail.com>
2020-07-11 22:34:21 +0300

    link to fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,16 +8,16 @@ class Navbar extends React.Component {
         return (
                 <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-white" id="main-navbar">
                     <div className="container">
-                        <Link to={`/`} className="navbar-brand">
+                        <Link to="/" className="navbar-brand">
                             <img src={logo} className="d-inline-block align-top" alt="logo"/>
                         </Link>
                         <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
                             <div className="navbar-nav">
-                                <a href={`/#how-it-works`} className="nav-item nav-link font-weight-bold active" >HOW IT WORKS</a>
-                                <Link to={`/menu`} className="nav-item nav-link font-weight-bold active">MENU</Link>
+                                <a href="/#how-it-works" className="nav-item nav-link font-weight-bold active" >HOW IT WORKS</a>
+                                <Link to="/menu" className="nav-item nav-link font-weight-bold active">MENU</Link>
                             </div>
                         </div>
-                        <Link to={`/`} className="btn btn-lg orderBtn" role="button">Start Order</Link>
+                        <Link to="/" className="btn btn-lg orderBtn" role="button">Start Order</Link>
                     <ShoppingCart/>
                     </div>
                 </nav>
